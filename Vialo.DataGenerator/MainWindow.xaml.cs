3fc4dcc20a9cb103586f906be7fcce4c947bf4a2
Yong <wynnsu@gmail.com>
2017-04-25 08:26:09 -0700

    Add project files.

---
+++ Vialo.DataGenerator/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vialo.ImageProcessing;
+
+namespace Vialo.DataGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string path = "";
+        VialoImage vImage;
+        int w, h;
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.DefaultExt = "bmp";
+            if (ofd.ShowDialog() == true)
+            {
+                path = ofd.FileName;
+                vImage = new VialoImage();
+                vImage.Init(path);
+                BitmapImage bmpSource = new BitmapImage(new Uri(path));
+                imgTarget.Source = bmpSource;
+                w = bmpSource.PixelWidth;
+                h = bmpSource.PixelHeight;
+                InitGrid(32);
+                imgTarget.Visibility = Visibility.Hidden;
+            }
+        }
+
+        List<Bitmap> fragList = new List<Bitmap>();
+
+        private void InitGrid(int size)
+        {
+            fragList = vImage.Fragment(size);
+            StackPanel innerStack;
+            ScaleTransform scale = new ScaleTransform(2.0, 2.0);
+            for (int i = 0; i < h / size; i++)
+            {
+                innerStack = new StackPanel { Orientation = Orientation.Horizontal };
+                for (int j = 0; j < w / size; j++)
+                {
+                    Bitmap b = fragList[(size - 1) * i + j];
+                    CheckBox cb = new CheckBox();
+                    cb.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
+                    cb.RenderTransform = scale;
+                    cb.Margin = new Thickness(6);
+                    ImageBrush brush = new ImageBrush();
+                    MemoryStream ms = new MemoryStream();
+                    b.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    ms.Seek(0, SeekOrigin.Begin);
+                    image.StreamSource = ms;
+                    image.EndInit();
+                    brush.ImageSource = image;
+                    cb.Background = brush;
+                    innerStack.Children.Add(cb);
+                }
+                container.Children.Add(innerStack);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(path))
+            {
+                string noisePath = System.IO.Path.GetDirectoryName(path) + @"/0/";
+                string backgroundPath = System.IO.Path.GetDirectoryName(path) + @"/1/";
+                if (!Directory.Exists(noisePath))
+                {
+                    Directory.CreateDirectory(noisePath);
+                }
+                if (!Directory.Exists(backgroundPath))
+                {
+                    Directory.CreateDirectory(backgroundPath);
+                }
+                int countNoise = Directory.GetFiles(noisePath).Length;
+                int countBackground = Directory.GetFiles(backgroundPath).Length;
+                int idx = 0;
+                foreach (StackPanel panel in container.Children)
+                {
+                    foreach (CheckBox cb in panel.Children)
+                    {
+                        if (cb.IsChecked == true)
+                        {
+                            fragList[idx].Save(noisePath + countNoise + ".bmp");
+                            countNoise++;
+                        }
+                        else
+                        {
+                            fragList[idx].Save(backgroundPath + countBackground + ".bmp");
+                            countBackground++;
+                        }
+                        idx++;
+                    }
+                }
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ Vialo.ImageProcessing/VialoImage.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using static Vialo.ImageProcessing.Helper;
+
+namespace Vialo.ImageProcessing
+{
+    public class VialoImage
+    {
+        private Bitmap bmp;
+
+        public Image GetImage()
+        {
+            return bmp;
+        }
+
+        public VialoImage Init(Image image)
+        {
+            bmp = new Bitmap(image);
+            return this;
+        }
+
+        public VialoImage Init(string path)
+        {
+            bmp = new Bitmap(path);
+            return this;
+        }
+
+        public List<Bitmap> Fragment(int size)
+        {
+            var list = new List<Bitmap>();
+            var w = bmp.Width;
+            var h = bmp.Height;
+            var hMargin = w % size / 2;
+            var vMargin = h % size / 2;
+
+            for (var i = vMargin; i < h - vMargin; i += size)
+            {
+                for (var j = hMargin; j < w - hMargin; j += size)
+                {
+                    list.Add(bmp.Clone(new Rectangle(j, i, size, size), bmp.PixelFormat));
+                }
+            }
+            return list;
+        }
+
+        public VialoImage BooleanEdgeDetectionFilter(
+                                BooleanFilterType filterType,
+                                float redFactor = 1.0f,
+                                float greenFactor = 1.0f,
+                                float blueFactor = 1.0f,
+                                byte threshold = 0)
+        {
+            Bitmap result = Helper.BooleanEdgeDetectionFilter(bmp, filterType, redFactor, greenFactor, blueFactor, threshold);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage Contrast(int threshold)
+        {
+            Bitmap result = Helper.Contrast(bmp, threshold);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage MarkForeground()
+        {
+            Bitmap result = Helper.MarkForeground(bmp);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage MarkForeground(int matrixSize)
+        {
+            Bitmap result = Helper.MarkForeground(bmp, matrixSize);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage ConvolutionFilter(double[,] filterMatrix,
+                                          double factor = 1,
+                                               int bias = 0,
+                                     bool grayscale = false)
+        {
+            Bitmap result = Helper.ConvolutionFilter(bmp, filterMatrix, factor, bias, grayscale);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage ConvolutionFilter(double[,] xFilterMatrix,
+                                        double[,] yFilterMatrix,
+                                              double factor = 1,
+                                                   int bias = 0,
+                                         bool grayscale = false)
+        {
+            Bitmap result = Helper.ConvolutionFilter(bmp, xFilterMatrix, yFilterMatrix, factor, bias, grayscale);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage PrewittFilter(bool grayscale = true)
+        {
+            Bitmap result = Helper.PrewittFilter(bmp, grayscale);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage GaussianFilter(bool grayscale = true)
+        {
+            Bitmap result = Helper.GaussianFilter(bmp, grayscale);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage DilateAndErodeFilter(
+                           int matrixSize,
+                           //MorphologyType morphType,
+                           bool applyBlue = true,
+                           bool applyGreen = true,
+                           bool applyRed = true)
+        {
+            Bitmap result = Helper.DilateAndErodeFilter(bmp, matrixSize, applyBlue, applyGreen, applyRed);
+            bmp = result;
+            return this;
+        }
+
+        public VialoImage MedianFilter(int matrixSize,
+                                              int bias = 0,
+                                    bool grayscale = false)
+        {
+            Bitmap result = Helper.MedianFilter(bmp, matrixSize, bias, grayscale);
+            bmp = result;
+            return this;
+        }
+        public VialoImage GradientBasedEdgeDetectionFilter(
+                                EdgeFilterType filterType,
+                                int derivativeLevel,
+                                float redFactor = 1.0f,
+                                float greenFactor = 1.0f,
+                                float blueFactor = 1.0f,
+                                byte threshold = 0)
+        {
+            Bitmap result = Helper.GradientBasedEdgeDetectionFilter(bmp, filterType, derivativeLevel, redFactor, greenFactor, blueFactor, threshold);
+            bmp = result;
+            return this;
+        }
+        public void Save(string path)
+        {
+            bmp.Save(path);
+        }
+    }
+}
